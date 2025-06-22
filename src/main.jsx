34e3b16208cc705352cdb6f9bad381bfee2eb778
MSAydogdu <sami@logithink.com.tr>
2025-06-23 02:57:51 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
-
-import React from 'https://esm.sh/react';
-import ReactDOM from 'https://esm.sh/react-dom/client';
-import VergiSimulasyonuTablosu from './VergiSimulasyonuDetay.jsx';
+// React and ReactDOM are loaded globally via CDN
+// VergiSimulasyonuTablosu is defined in simulator.jsx
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
